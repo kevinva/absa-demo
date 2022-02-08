97da193c01e3452c38093bbb3cc4e0c2c31db241
hezhi <hezhi@joyy.com>
2022-02-08 12:14:41 +0800

    test

--- atae_lstm.py
+++ atae_lstm.py
@@ -8,10 +8,10 @@ class ATAE_LSTM(nn.Module):
         pass
 
 
-class DynamicLSTM(nn.Module):
+class DynamicRNN(nn.Module):
 
     def __init__(self, input_size, hidden_size, num_layers=1, bias=True, batch_first=True, dropout=0, bidirectional=False, only_use_last_hidden_state=False, rnn_type='LSTM'):
-        super(DynamicLSTM, self).__init__()
+        super(DynamicRNN, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.num_layers = num_layers
@@ -47,16 +47,25 @@ class DynamicLSTM(nn.Module):
                               dropout=dropout,
                               bidirectional=bidirectional)
 
+
     def forward(self, x, x_len):
         # x为一个batch的样本序列
         # x_len为x中每个样本的长度序列
+        print('rnn forward....')
+        print('x raw: ', x)
+        print('x len: ', x_len)
 
         x_sort_idx = torch.sort(-x_len)[1].long() # x由大到小的排列的索引号
+        print('x_sort_idx: ', x_sort_idx)
+
         x_unsort_idx = torch.sort(x_sort_idx)[1].long()
         x_len = x_len[x_sort_idx]
         x = x[x_sort_idx]
+        print('x_sort: ', x)
 
+        # pack_padded_sequence会把RNN每一步要处理的batch中每个样本的数据结构整理好，不用输入embedding层之后自行调整数据结构
         x_emb_p = pack_padded_sequence(x, x_len, batch_first=self.batch_first)
+        print('x_emb_p: ', x_emb_p)
 
         if self.rnn_type == 'LSTM':
             out_pack, (ht, ct) = self.RNN(x_emb_p, None)
@@ -70,9 +79,16 @@ class DynamicLSTM(nn.Module):
         if self.only_use_last_hidden_state:
             return ht
         else:
+            print('out_pack: ', out_pack)
             out = pad_packed_sequence(out_pack, batch_first=self.batch_first)
+            print('out1: ', out)
+
             out = out[0]
+            print('out2: ', out)
             out = out[x_unsort_idx]
+            print('out3: ', out)
+
+            print('ct: ', ct)
             if self.rnn_type == 'LSTM':
                 ct = torch.transpose(ct, 0, 1)[x_unsort_idx]
                 ct = torch.transpose(ct, 0, 1)
